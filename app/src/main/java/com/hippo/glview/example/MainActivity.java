27b3a1501ab913354109503e66f21f9a0124a74f
seven332 <seven332@163.com>
2016-08-28 19:24:13 +0800

    Update example

--- app/src/main/java/com/hippo/glview/example/MainActivity.java
+++ app/src/main/java/com/hippo/glview/example/MainActivity.java
@@ -1,18 +1,43 @@
 package com.hippo.glview.example;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.hippo.glview.view.GLRootView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends GLActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GLRootView glRootView = (GLRootView) findViewById(R.id.gl_root);
+        final GLRootView glRootView = getGLRootView();
         glRootView.setContentPane(new GLTestView());
+    }
+
+    @Override
+    protected int getGLRootViewId() {
+        return R.id.gl_root;
+    }
+
+    @Override
+    public void onGLThreadStarts() {
+        Log.d("TAG", "onGLThreadStarts");
+    }
+
+    @Override
+    public void onGLThreadExits() {
+        Log.d("TAG", "onGLThreadExits");
+    }
+
+    @Override
+    public void onGLThreadPause() {
+        Log.d("TAG", "onGLThreadPause");
+    }
+
+    @Override
+    public void onGLThreadResume() {
+        Log.d("TAG", "onGLThreadResume");
     }
 }
